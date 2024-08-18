d4fe85b432c38ca1a74b2d5b96e200fbecf06553
bgilross <bgilross@gmail.com>
2024-08-17 17:57:21 -0700

    multi decks logic

--- src/MainPage.js
+++ src/MainPage.js
@@ -2,47 +2,50 @@ import { useState, useEffect } from 'react'
 import uniqid from 'uniqid'
 
 const MainPage = () => {
-  const [deck, setDeck] = useState([])
   const [decks, setDecks] = useState([])
+  const [currentCard, setCurrentCard] = useState(null)
+  const [deckNumber, setDeckNumber] = useState(0)
 
   useEffect(() => {
-    console.log({ deck })
-  }, [deck])
+    console.log({ decks })
+  }, [decks])
 
-  const createDeck = () => {
-    let tempDeck = []
-
-    tempDeck.push(createSuit('hearts'))
-    console.log(tempDeck)
-    tempDeck.push(createSuit('spades'))
-    console.log(tempDeck)
-    tempDeck.push(createSuit('diamonds'))
-    console.log(tempDeck)
-    tempDeck.push(createSuit('clubs'))
-    console.log(tempDeck)
-    setDeck(tempDeck)
-
-    console.log({ deck })
+  const handleDeckNumberChange = (event) => {
+    setDeckNumber(Number(event.target.value))
   }
 
-  const createSuit = (suit) => {
-    let newSuit = []
-    // const newCard = {}\
-    let counter = 13
+  const createDecks = () => {
+    let newDecks = []
 
-    while (counter > 0) {
-      newSuit.push({
-        id: uniqid(),
-        key: uniqid(),
-        number: counter,
-        suit: suit,
-        played: false,
-      })
-      console.log({ newSuit })
-      counter--
+    for (let i = 0; i < deckNumber; i++) {
+      const newDeck = createDeck()
+      newDecks.push(...newDeck) // Spread and push to flatten the array here
     }
+    setDecks(newDecks)
+  }
 
-    return newSuit
+  const createDeck = () => {
+    const suits = ['hearts', 'spades', 'diamonds', 'clubs']
+    return suits.flatMap((suit) =>
+      Array.from({ length: 13 }, (_, i) => ({
+        id: uniqid(),
+        number: i + 1,
+        suit: suit,
+      }))
+    )
+  }
+
+  const playCard = () => {
+    if (decks.length > 0) {
+      const index = Math.floor(Math.random() * decks.length)
+      const card = decks[index]
+      const newDeck = [...decks.slice(0, index), ...decks.slice(index + 1)]
+      setDecks(newDeck)
+      setCurrentCard(card)
+    } else {
+      const card = { number: 'Out of Cards', suit: 'please deal again' }
+      setCurrentCard(card)
+    }
   }
 
   return (
@@ -51,11 +54,23 @@ const MainPage = () => {
         <h1 className="f-6 flex items-center justify-center">BLACKJACK</h1>
       </div>
       <div className="vh-75 flex flex-column pa4">
-        Body
-        <div className="ba bw1 br4 h-75 pa3 flex items-center justify-center">
-          <label htmlFor="deckNumber">Deck Number</label>
-          <input type="number" name="deckNumber" id="deckNumber" />
-          <button onClick={createDeck}>Create Decks</button>
+        <div className="ba bw1 br4 h-75 pa3 flex flex-wrap items-center justify-center">
+          <div className="w-100">
+            <label htmlFor="deckNumber">Deck Number</label>
+            <input
+              type="number"
+              value={deckNumber}
+              onChange={handleDeckNumberChange}
+              name="deckNumber"
+              id="deckNumber"
+            />
+            <button onClick={createDecks}>Create Decks</button>
+          </div>
+          <button onClick={playCard}>Play Card</button>
+          <div className="h4 w4 ba bw1">
+            <p>Number: {currentCard?.number}</p>
+            <p>Suit: {currentCard?.suit}</p>
+          </div>
         </div>
       </div>
       <div className="ba bw1 f1 ma2">Footer</div>
