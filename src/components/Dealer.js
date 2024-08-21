dd4eda826905ce352e213a231f150945529289d1
bgilross <bgilross@gmail.com>
2024-08-20 18:41:22 -0700

    add back card to dealer

--- src/components/Dealer.js
+++ src/components/Dealer.js
@@ -1,9 +1,23 @@
 import Card from './Card'
-
+import CardBack from '../images/cards/CardBack.png'
 const Dealer = ({ dealerHand }) => {
-  const displayHand = dealerHand.map((card) => (
-    <Card key={card.id} card={card.card} suit={card.suit} />
-  ))
+  const displayHand = dealerHand.map((card, index) => {
+    console.log(dealerHand)
+    console.log(card, index)
+    if (index != 1) {
+      return <Card key={card.id} card={card.card} suit={card.suit} />
+    } else {
+      return (
+        <img
+          key={index}
+          src={CardBack}
+          alt="card back"
+          height="150"
+          width="100"
+        />
+      )
+    }
+  })
 
   return <div className="flex">{displayHand}</div>
 }
