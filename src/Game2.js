4921f2da3105bf308e38a20c418ddcbba8a04f24
bgilross <bgilross@gmail.com>
2024-08-26 15:23:38 -0700

    gh-pages test

--- src/Game2.js
+++ src/Game2.js
@@ -4,6 +4,7 @@ import PlayArea2 from './components/PlayArea2'
 const Game2 = () => {
   return (
     <div className="min-vh-100 max-vh-100 bg-dark-green pa1">
+      <h1>gh-pages-branch</h1>
       <Header />
       <PlayArea2 />
     </div>
